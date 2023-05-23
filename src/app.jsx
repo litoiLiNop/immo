210506e7755da3207903e4e17877959f51cedadc
litoilinop <litoilinop@gmail.com>
2023-05-23 05:35:15 +0100

    css

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import "./App.css";
+import "./app.css";
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
